aa7cc6e888d8d1897908e9bd60ec5be19e9072cf
OneGiantNick <47775170+OneGiantNick@users.noreply.github.com>
2019-02-20 11:50:18 +0800

    Create q4_sum_digits.py

---
+++ q4_sum_digits.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter integer between 1 and 1000"))
+#sum of digits
+if len(num1) = 0:
+    sumdigit = num1[0]
+    elif len(num1) = 1:
+        sumdigit = num1[0] + num1[1]
+        else len(num1) = 2:
+            sumdigit = num1[0] + num1[2] + num1[3]
+print("sum of digits is", sumdigit)
