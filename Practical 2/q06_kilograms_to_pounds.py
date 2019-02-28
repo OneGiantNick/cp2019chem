7ed53c653073143431d56957169b4e340c18919d
OneGiantNick <47775170+OneGiantNick@users.noreply.github.com>
2019-02-28 08:45:20 +0800

    Update q06_kilograms_to_pounds.py

--- Practical 2/q06_kilograms_to_pounds.py
+++ Practical 2/q06_kilograms_to_pounds.py
@@ -1,4 +1,14 @@
-print('kilogrammes to pounds conversion table')
-for i in range(0, 11):
-    print('|', i, '|', '{:.1f}'.format(i * 2.2), '|')
-    i + 1
+
+def print_table(x, y):
+    print('K1ilogrammes to pounds conversion table')
+    print("---------------------------------------------------------------")
+    print("Kilogrammes\t\t\tPounds")
+    for i in range(x, y + 1):
+        print("%d\t\t\t\t\t%s" % (i, '{:.1f}'.format(i * 2.2)))
+        i + 1
+    print("---------------------------------------------------------------")
+
+
+x = int(input('First integer in table '))
+y = int(input('Last integer in table '))
+print_table(x, y)
