d429ae4c4dae457a1747596ec43c578103677525
OneGiantNick <47775170+OneGiantNick@users.noreply.github.com>
2019-03-03 14:45:52 +0800

    Update q05_find_month_days.py

--- Practical 2/q05_find_month_days.py
+++ Practical 2/q05_find_month_days.py
@@ -1,40 +1,7 @@
 
+from calendar import monthrange
 
-# Finding values
-year = int(input('Enter the year here '))
-month = str(input('Enter the month here '))
-month = month.lower()
+month = int(input("Enter month: "))
+year = int(input("Enter year: "))
 
-# Dictionary
-day_month = {
-    'january': 31,
-    'february': 28,
-    'march': 31,
-    'april': 30,
-    'may': 31,
-    'june': 30,
-    'july': 31,
-    'august': 31,
-    'september': 30,
-    'october': 31,
-    'november': 30,
-    'december': 31,
-}
-# if leap year is february
-february = {
-    'february': 29,
-}
-
-
-def february_leap(month, year):
-    if month == 'february' and year % 4 == 0 and year % 100 != 0:
-        for key, value in february.items():
-            print(value, 'days in', key, year)
-    elif month == 'february' and year % 400 == 0:
-        for key, value in february.items():
-            print(value, 'days in', key, year)
-    else:
-        print(day_month[month], 'days in', month, year)
-
-
-february_leap(month, year)
+print(monthrange(year, month)[1])
