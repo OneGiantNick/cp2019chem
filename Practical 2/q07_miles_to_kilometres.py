6c751509a89a28065a29b57762f688826417ece6
OneGiantNick <47775170+OneGiantNick@users.noreply.github.com>
2019-03-04 09:54:36 +0800

    Update q07_miles_to_kilometres.py

--- Practical 2/q07_miles_to_kilometres.py
+++ Practical 2/q07_miles_to_kilometres.py
@@ -1,1 +1,14 @@
+def print_table(x, y):
+    print('Kilometres and Miles conversion table')
+    print("---------------------------------------------------------------")
+    print("Miles\t\t\tKilometres\t\t\tKilometres\t\t\tMiles")
+    for i in range(x, y + 1):
+        print("%s\t\t\t\t%s\t\t\t\t\t%s\t\t\t\t\t%s" %
+              ('{:.1f}'.format(i), '{:.1f}'.format(i * 1.609), '{:.1f}'.format(i), '{:.1f}'.format(i / 1.609)))
+        i + 1
+    print("---------------------------------------------------------------")
 
+
+x = int(input('First integer in table '))
+y = int(input('Last integer in table '))
+print_table(x, y)
