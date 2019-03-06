b2222ef68396ea3aecafa57454c01231f1227be0
OneGiantNick <47775170+OneGiantNick@users.noreply.github.com>
2019-03-06 11:32:34 +0800

    Update q05_find_month_days.py

--- Practical 2/q05_find_month_days.py
+++ Practical 2/q05_find_month_days.py
@@ -1,10 +1,52 @@
-import calendar
-c=calendar.TextCalendar(calendar.SUNDAY)
-year=eval(input("Enter year: "))
-month=eval(input("Enter month: "))
-days=0
-for i in c.itermonthdays(year, month):
-    if i!=0:
-        days+=1
 
-print(days)
+# Finding values
+year = int(input('Enter the year here '))
+month = str(input('Enter the month here '))
+
+# Dictionary
+day_month = {
+    '1': 31,
+    '2': 28,
+    '3': 31,
+    '4': 30,
+    '5': 31,
+    '6': 30,
+    '7': 31,
+    '8': 31,
+    '9': 30,
+    '10': 31,
+    '11': 30,
+    '12': 31,
+}
+month_year = {
+    '1': 'January',
+    '2': 'February',
+    '3': 'March',
+    '4': 'April',
+    '5': 'May',
+    '6': 'June',
+    '7': 'July',
+    '8': 'August',
+    '9': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+}
+# if leap year is february
+february = {
+    'february': 29,
+}
+
+
+def february_leap(month, year):
+    if month == 2 and year % 4 == 0 and year % 100 != 0:
+        for key, value in february.items():
+            print(value, 'days in', key, year)
+    elif month == 2 and year % 400 == 0:
+        for key, value in february.items():
+            print(value, 'days in', key, year)
+    else:
+        print(day_month[month], 'days in', month_year[month], year)
+
+
+february_leap(month, year)
